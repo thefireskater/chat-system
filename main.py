0e1f8966127249672f8eb32358ec846fa7435217
thefireskater <114769322+thefireskater@users.noreply.github.com>
2024-09-24 18:16:01 -0600

    Initial support for sending messages

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
 from fastapi import FastAPI, WebSocket
 from fastapi.responses import HTMLResponse
+import json
+from typing import Dict
 
+class ConnectionManager:
+    def __init__(self):
+        self.active_connections: Dict[str, WebSocket] = {}
+
+    async def connect(self, websocket: WebSocket, user_id: str):
+        self.active_connections[user_id] = websocket
+
+    def disconnect(self, user_id: str):
+        del self.active_connections[user_id]
+
+    async def send_direct_message(self, message: str, user_id: str):
+        await self.active_connections[user_id].send_text(message)
 
 app = FastAPI()
+manager = ConnectionManager()
 
 
 html = """
@@ -45,9 +60,14 @@ async def get():
     return HTMLResponse(html)
 
 
-@app.websocket("/ws")
-async def websocket_endpoint(websocket: WebSocket):
+@app.websocket("/ws/{user_id}")
+async def websocket_endpoint(websocket: WebSocket, user_id: str):
     await websocket.accept()
     while True:
-        data = await websocket.receive_text()
-        await websocket.send_text(f"Message text was: {data}")
+        data_json = await websocket.receive_text()
+        data = json.loads(data_json)
+        if data['type'] == 'heartbeat':
+            print(f'Heartbeat: {user_id}')
+            await manager.connect(websocket, user_id)
+        elif data['type'] == 'message':
+            await manager.send_direct_message(data['message'], data['receiverId'])
