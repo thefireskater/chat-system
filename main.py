c76a8bce63cbc5054adf4149134a46ac27a1b0cb
thefireskater <114769322+thefireskater@users.noreply.github.com>
2024-09-24 19:43:05 -0600

    Switch to Jinja2 template

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from fastapi import FastAPI, WebSocket
+from fastapi import FastAPI, WebSocket, Request
 from fastapi.responses import HTMLResponse
 import json
 from typing import Dict
+from fastapi.templating import Jinja2Templates
 
 class ConnectionManager:
     def __init__(self):
@@ -18,51 +19,14 @@ class ConnectionManager:
 
 app = FastAPI()
 manager = ConnectionManager()
+templates = Jinja2Templates(directory="templates")
 
 
-html = """
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Chat</title>
-    </head>
-    <body>
-        <h1>WebSocket Chat</h1>
-        <form action="" onsubmit="sendMessage(event)">
-            <label for="receiverId">Receiver ID</label>
-            <input type="text" id="receiverId" autocomplete="off"/>
-            <label for="messageText">Message</label>
-            <input type="text" id="messageText" autocomplete="off"/>
-            <button>Send</button>
-        </form>
-        <ul id='messages'>
-        </ul>
-        <script>
-            var ws = new WebSocket("ws://localhost:8000/ws/{user_id}");
-            ws.onmessage = function(event) {
-                var messages = document.getElementById('messages')
-                var message = document.createElement('li')
-                var content = document.createTextNode(event.data)
-                message.appendChild(content)
-                messages.appendChild(message)
-            };
-            function sendMessage(event) {
-                var receiverId = document.getElementById("receiverId")
-                var messageText = document.getElementById("messageText")
-                ws.send(JSON.stringify({"type": "message", "receiverId": receiverId.value, "message": messageText.value}))
-                messageText.value = ''
-                event.preventDefault()
-            }
-        </script>
-    </body>
-</html>
-"""
-
-
-@app.get("/user/{user_id}")
-async def get(user_id: str):
-    # todo not secure, use jinja2 template
-    return HTMLResponse(html.replace("{user_id}", user_id))
+@app.get("/user/{user_id}", response_class=HTMLResponse)
+async def get_chat_page(request: Request, user_id: str):
+    return templates.TemplateResponse(
+        request=request, name="chat.html", context={"user_id": user_id}
+    )
 
 
 @app.websocket("/ws/{user_id}")
